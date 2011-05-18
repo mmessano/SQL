d82d964b231f3df3b0c72f8c34184c772d99ea39
Michael J. Messano <mmessano@dexma.com>
2011-05-18 13:14:32 -0500

    Deleted files manually.

--- query_clients_and_DSNS.sql
+++
@@ -1,5 +0,0 @@
-SELECT c.client_name, ds.machine_name, ds.database_name, ds.environment, ds.client_id
-FROM 
-	[ops].[dbo].[client_data_sources] ds inner join
-	[ops].[dbo].[clients] c on c.client_id = ds.client_id
-order by client_name
--- tran_logs.sql
+++
@@ -1,8 +0,0 @@
-select * from sql_spacestats where server_name IN ('Folsom', 'bellatrix') and report_date > '4/13/2007' order by 3, 11
-
-
-
-
-dbcc sqlperf(logspace)
-
-sp_helpfile
